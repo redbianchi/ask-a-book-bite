2c922980425abc6c7ac71178da1795c48f05bd8a
Christopher Chaput <christopher.chaput@gmail.com>
2023-07-10 14:37:34 -0400

    index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const apiKey = process.env.OPENAI_API_KEY;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname, '/index.html');
 });
 
 app.get('/favicon.ico', (req, res) => {
